ff25b94c0d374c2bfad5b8d4c41e4f499abefe43
Richard Luther <heliwr@gmail.com>
2012-06-23 15:05:26 -0700

    added check for CommandBook mute to /me

--- src/com/github/Heliwr/IgnoreChat/Ignore.java
+++ src/com/github/Heliwr/IgnoreChat/Ignore.java
@@ -3,13 +3,22 @@ package com.github.Heliwr.IgnoreChat;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import com.sk89q.commandbook.CommandBook;
 
 public class Ignore extends JavaPlugin {
     private Map<String, List<String>> ignoreList = new HashMap<String, List<String>>();
-
+    CommandBook commandbook =  (CommandBook) Bukkit.getServer().getPluginManager().getPlugin("CommandBook");    
+    boolean cmdbook = false;
+    
     public void onEnable() {
         // Chat listener
+        if(commandbook != null) {
+        	cmdbook = true;
+          }
+
     	getServer().getPluginManager().registerEvents(new IgnoreListener(this), this);
 
         // Setup commands
--- src/com/github/Heliwr/IgnoreChat/IgnoreListener.java
+++ src/com/github/Heliwr/IgnoreChat/IgnoreListener.java
@@ -2,28 +2,32 @@ package com.github.Heliwr.IgnoreChat;
 
 import java.util.*;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.*;
 import org.bukkit.event.player.PlayerChatEvent;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 
+import com.sk89q.commandbook.CommandBook;
+import com.sk89q.commandbook.session.AdministrativeSession;
+import com.sk89q.commandbook.session.SessionComponent;
+
 class IgnoreListener implements Listener {
     private final Ignore plugin;
-
     public IgnoreListener(Ignore p) {
         plugin = p;
     }
 
     @EventHandler(priority = EventPriority.LOWEST)
     public void onPlayerChat(PlayerChatEvent event) {
-        if (event.isCancelled()) return;
+        if(event.isCancelled()) return;
 
         Map<String, List<String>> ignoreList = plugin.getList();
         String p = event.getPlayer().getName();
         Player player = event.getPlayer();
 
-        for (Iterator<Player> it = event.getRecipients().iterator(); it.hasNext();) {
+        for(Iterator<Player> it = event.getRecipients().iterator(); it.hasNext();) {
             Player r = it.next();
             List<String> recipientIgnores = ignoreList.get(r.getName());
             boolean recipientIgnoresPlayer = false;
@@ -31,57 +35,74 @@ class IgnoreListener implements Listener {
             if(recipientIgnores != null) {
                 for(String i : recipientIgnores) {
                 	if(i.startsWith("group.")) {
-                		recipientIgnoresPlayer = player.hasPermission(i) && !player.hasPermission("ignore.block");
+                		recipientIgnoresPlayer = (player.hasPermission(i) || i.equalsIgnoreCase("group.*")) && !player.hasPermission("ignore.block");
                 	}
                 }
                 if(!recipientIgnoresPlayer) {
                     recipientIgnoresPlayer = recipientIgnores.contains(p);            	
                 }
-                if (recipientIgnoresPlayer) {
+                if(recipientIgnoresPlayer) {
                     it.remove();
                 }
             }
         }
     }
     
-    @EventHandler(priority = EventPriority.LOWEST)
+	@EventHandler(priority = EventPriority.LOWEST)
 	public void onPlayerCommandPreprocess(PlayerCommandPreprocessEvent event) {
-		if (event.isCancelled()) return;
+    	if(event.isCancelled()) return;
 
 		String message = event.getMessage();
         Map<String, List<String>> ignoreList = plugin.getList();
         String p = event.getPlayer().getName();
 		Player player = event.getPlayer();
-
-		if (message.toLowerCase().startsWith("/me ")) {
-			String s = message.substring(message.indexOf(" ")).trim();
-			
-			for (Player r : plugin.getServer().getOnlinePlayers()) {
-	            List<String> recipientIgnores = ignoreList.get(r.getName());
-	            boolean recipientIgnoresPlayer = false;
-	            
-	            if(recipientIgnores != null) {
-		            for(String i : recipientIgnores) {
-		            	if(i.startsWith("group.")) {
-		            		if(player.hasPermission(i) && !player.hasPermission("ignore.block")) {
-		            			recipientIgnoresPlayer = true;
-		            		}
-		            	}
-		            }
-		            if(!recipientIgnoresPlayer) {
-		                recipientIgnoresPlayer = recipientIgnores.contains(p);            	
-		            }	            	
-	            }
-	            if (!recipientIgnoresPlayer) {
-	            	r.sendMessage(ChatColor.LIGHT_PURPLE + "* " + player.getName() + " " + s);
-	            }
-			}
-			event.setCancelled(true);
+		String[] ignoredCmds={"/me","/msg","/login","/register","/bal","/fly","/stopfly",
+				"/spawn","/serenity","/td","/tn","/where","/spy","/c","/p","/r","/u","/e",
+				"/o","/giveme","/lease","/expand","/rent","/pay","/invite","/join","/summon",
+				"/tphere","/tp","/goto","/back","/tpdeny","/sethome","/delhome","/home",
+				"/setwarp","/clean","/tell","/we"};
+		int index = message.indexOf(' ');
+		if(index < 0) index = message.length();
+		String cmd = message.substring(0, index);
+		
+		if(!Arrays.asList(ignoredCmds).contains(cmd)) {
+			System.out.println("[" + p + " in " + player.getLocation().getWorld().getName() + "]:" + message);		
 		}
 		
-		
-		if (message.toLowerCase().startsWith("/msg ") || message.toLowerCase().startsWith("/tell ")) {
-			if (!message.contains(" ")) {
+		if(cmd.equalsIgnoreCase("/me")) {
+			String s = message.substring(message.indexOf(" ")).trim();
+			boolean muted = false;
+			
+			if(plugin.cmdbook) {
+				muted = CommandBook.inst().getComponentManager().getComponent(SessionComponent.class).getSession(AdministrativeSession.class, player).isMute();
+			}
+			
+			if(!muted || !plugin.cmdbook) {
+				for(Player r : plugin.getServer().getOnlinePlayers()) {
+		            List<String> recipientIgnores = ignoreList.get(r.getName());
+		            boolean recipientIgnoresPlayer = false;
+		            
+		            if(recipientIgnores != null) {
+			            for(String i : recipientIgnores) {
+			            	if(i.startsWith("group.")) {
+			            		if(player.hasPermission(i) && !player.hasPermission("ignore.block")) {
+			            			recipientIgnoresPlayer = true;
+			            		}
+			            	}
+			            }
+			            if(!recipientIgnoresPlayer) {
+			                recipientIgnoresPlayer = recipientIgnores.contains(p);            	
+			            }	            	
+		            }
+		            if(!recipientIgnoresPlayer) {
+		            	r.sendMessage(ChatColor.LIGHT_PURPLE + "* " + player.getName() + " " + s);
+		            }
+				}
+				event.setCancelled(true);
+				Bukkit.getConsoleSender().sendMessage(ChatColor.LIGHT_PURPLE + "* " + player.getName() + " " + s);				
+			}
+		} else if(cmd.equalsIgnoreCase("/msg ") || cmd.equalsIgnoreCase("/tell ")) {
+			if(!message.contains(" ")) {
 				player.sendMessage("ErrMsgFormat");
 				event.setCancelled(true);
 				return;
@@ -93,11 +114,11 @@ class IgnoreListener implements Listener {
 			List<Player> recipients;
 			Player r;
 
-			if (name.substring(0, 1).equalsIgnoreCase("@")) {
+			if(name.substring(0, 1).equalsIgnoreCase("@")) {
 				r = plugin.getServer().getPlayerExact(name.substring(1));
 			} else {
 				recipients = plugin.getServer().matchPlayer(name);
-				if (recipients.size() > 1) {
+				if(recipients.size() > 1) {
 					player.sendMessage(ChatColor.RED + "Too Many Players Found");
 					event.setCancelled(true);
 					return;
@@ -106,7 +127,6 @@ class IgnoreListener implements Listener {
 					event.setCancelled(true);
 					return;
 				}
-
 				r = recipients.get(0);
 			}	
             
@@ -124,12 +144,11 @@ class IgnoreListener implements Listener {
                 if(!recipientIgnoresPlayer) {
                     recipientIgnoresPlayer = recipientIgnores != null && (recipientIgnores.contains(p));            	
                 }			
-                if (recipientIgnoresPlayer) {
+                if(recipientIgnoresPlayer) {
     				player.sendMessage(ChatColor.RED + "Player" + name + "is ignoring you.");
                 	event.setCancelled(true);
                 	return;
                 }
-            	
             }
 		}
     }
